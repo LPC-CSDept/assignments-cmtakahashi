71ca46a0aaeea88daaa3bcbef55e93e5ed0fb2c5
cmtakahashi <ctakahashi08@gmail.com>
2021-09-09 04:02:06 -0700

    Added comments and changed to an array of structs

--- readfile.cpp
+++ readfile.cpp
@@ -3,9 +3,10 @@
 
 using namespace std;
 
-struct Student { //create struct to define the data
+// defines a structure for students to store the contents of a text file
+struct Student {
   int id;
-  string sname;
+  char sname[20];
   double score[2];
   double sum;
   double avg;
@@ -14,10 +15,25 @@ struct Student { //create struct to define the data
 int main()
 {
   ifstream ifs;
-	Student s[10];
-	ifs.open("students.txt");
+	ofstream ofs;
+  Student s[10];
 
-	for( int i=0;i<10; i++) //reads each line in the file
+
+	ifs.open("students.txt"); // opens the file
+    ofs.open("students.bin" ); // opens a binary file to write to
+
+  // get and print the number of lines in the file
+  int line_count = 0;
+    std::string line;
+    std::ifstream myfile("students.txt");
+
+    while (std::getline(myfile, line))
+        ++line_count;
+    std::cout << "Number of lines in the file: " << line_count << "\n";
+
+  
+	// stores the data in the struct
+	for( int i=0; i < line_count; i++)
 	{
 		ifs >> s[i].id;
 		ifs >> s[i].sname;
@@ -27,13 +43,19 @@ int main()
 		s[i].avg = s[i].sum / 2;
 	}
 
-	for( int i=0;i<10; i++) //outputs each line in the file to the console
+	// prints the stored data
+	for( int i=0;i< line_count; i++)
 	{
 		cout << s[i].id << "\t" ;
 		cout << s[i].sname << "\t";
 		cout << s[i].score[0] << " " << s[i].score[1] << "\t";
 		cout << s[i].sum << " " << s[i].avg << endl;
 	}
+
+	//writes the struct contents to the binary file
+
+	for( int i=0;i< line_count; i++)
+		ofs.write( (char *)&s, sizeof(s));
 
 
 }
--- readfile2.cpp
+++ readfile2.cpp
@@ -2,7 +2,7 @@
 #include <fstream>
 using namespace std;
 
-struct Student { //creates a parallel struct student to store the input information
+struct Student {
   int id;
   char sname[20];
   double score[2];
@@ -10,21 +10,78 @@ struct Student { //creates a parallel struct student to store the input informat
   double avg;
 };
 
-int main()
+Student s[10]; // defines an array of structs
+
+
+void findStudent(Student *s)
+  {
+    int userInput;
+    cout << "Enter a student ID or press 0 to quit: "; 
+    cin >> userInput;
+
+    while (userInput !=0) 
+    {
+      bool studentFound = false;
+      for (int i =0; i <= sizeof(s) + 1; i++)
+      {
+        if (userInput == s[i].id)
+        {
+          cout << "ID: " << s[i].id << "\t";
+          cout << "Name: " << s[i].sname << "\t";
+          cout << "Score1: " << s[i].score[0] << "\t";
+          cout << "Score2: " << s[i].score[1] << "\t";
+          cout << "Sum: " << s[i].sum << "\t";
+          cout << "Avg: " << s[i].avg << "\n";
+          studentFound = true;
+          break;
+        
+        }
+
+      
+      }
+	  // valiation message if no student found with matching ID
+      if (studentFound == false){
+      cout << "There is no student with ID: " << userInput << "\n";
+      cout << "Enter another student ID or press 0 to quit: ";
+      cin >> userInput;
+      }
+	  // allow user to query for another student
+      else if (studentFound == true){
+      cout << "Enter another student ID or press 0 to quit: ";
+      cin >> userInput;
+      }
+    while(!cin) // prompt the user to enter an integer for the ID
+    {
+        cout << "That was not a valid student ID!! Please enter an integer: ";
+        cin.clear();
+        cin.ignore();
+        cin >> userInput;
+    }
+
+    }
+    if (userInput == 0)
+      cout << "Bye!" << endl;
+  }
+
+
+
+int main()  
 {
 	ifstream ifs;
-	Student s;
 
-	ifs.open("students.bin"); //opens the binary file
+	ifs.open("students.bin"); // opens the binary file
 
-	ifs.read( (char *)&s, sizeof(s) ); //reads the information in the input file for the first student
+	ifs.read( (char *)&s, sizeof(s) ); // reads the file contents
 
-	//output the properties of Student s
-	cout << "ID " << s.id << "\t";
-	cout << "Name " << s.sname << "\t";
-	cout << "Score1 " << s.score[0] << "\t";
-	cout << "Score2 " << s.score[1] << "\t";
-	cout << "Sum " << s.sum << "\t";
-	cout << "Avg " << s.avg << "\n";
-
+  findStudent(s); // calls findStudent
+  
+  
 }
+
+
+
+
+
+
+
+
