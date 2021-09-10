feb1426057e28ccb751ddaf30d765995ab65601d
cmtakahashi <ctakahashi08@gmail.com>
2021-09-09 20:00:51 -0700

    modified the code to fit the desired function template

--- assignment8-1.cpp
+++ assignment8-1.cpp
@@ -2,41 +2,56 @@
 #include <algorithm> // needed to use the sort function.
 using namespace std;
 
+
+int binarySearch(int [], int, int);
+// 1st parameter : Integer Array
+// 2nd Parameter : Size of the array N
+// 3rd Parameter : Taget Value
+// Return value and type : the index of the array if the target is in the array. -1 if not found.
+
+
 int main()
 {
 	const int N = 10;
 	int numbers[N] = {10, 25, 30, 35, 40, 45, 55, 60, 65, 70 };
 	int target = 35;
-    int first, last, mid;
-    bool found = false;
+	int index;
 
-    first = 0;
-    last = N-1; 
-    mid = (first+last)/2;  
-  
 
+	index = binarySearch(numbers, N, target );
+	if ( index == -1)
+		cout << "Target not found \n";
+	else
+		cout << "Target found at index : " << index << endl;
+}
+
+int binarySearch(int numbers[], int N, int target)
+{
+	int first, last, mid;
+  bool found = false;
+  first = 0;
+  last = N-1;
+  mid = (first+last)/2;
 
   while (first <= last)
   {
-    mid = (first+last)/2;
+      mid = (first+last)/2;
 
-    if (numbers[mid] == target){
-      cout << "Found at index: " << mid << endl; 
-      found = true;
-      break;
-    }
+      if (numbers[mid] == target){
+        found = true;
+        return mid;
+        break;
+      }
 
-    else if (numbers[mid] < target) {
-      first = mid + 1;
-    }
-    else {
-      last = mid-1;
-    }
+      else if (numbers[mid] < target) {
+        first = mid + 1;
+      }
+    
+      else {
+        last = mid-1;
+      }
      
-
   }
   if (!found)
-    cout << "The target was not found in the values"; 
-  
-
+	  return -1;  
 }
