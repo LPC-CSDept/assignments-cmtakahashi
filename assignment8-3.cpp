d51bb8585ea40924b096f8bc48496562dac67a95
cmtakahashi <ctakahashi08@gmail.com>
2021-09-14 19:21:29 -0700

    added functions to sort and print

--- assignment8-3.cpp
+++ assignment8-3.cpp
@@ -1,11 +1,46 @@
 #include <iostream>
 using namespace std;
 
+int findMin(int [], int, int);
+void printArray(int [], int);
+
 int main()
 {
-    const int N = 10;
-    int numbers[N] = {};
-    
+	const int N = 10;
+	int numbers[N] = {25, 10, 15, 30, 35, 40, 45, 55,20,50};
+	int minidx;
 
+	printArray(numbers, N);
 
+	for(int i=0;i<N-1;i++)
+    {
+        minidx= findMin(numbers, N, i); // Find the i_th smallest number
+		swap(numbers[i], numbers[minidx]);
+	}
+
+	printArray(numbers, N);
+
+}
+
+int findMin(int numbers[], int N, int ith)
+{
+	int min, minidx, i;
+	min = numbers[ith];
+    for(i=ith ;i<N;i++)
+	{
+		if ( min > numbers[i] )
+		{
+			min = numbers[i];
+			minidx = i;
+		}
+	}
+	return minidx; // 1
+}
+
+void printArray(int a[], int x)
+{
+	for(int i = 0; i < x; i++){
+    cout << a[i] << "\t";
+  }
+  cout << "\n";
 }
