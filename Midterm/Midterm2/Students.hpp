ec7901fa4a0eb0d63c6847a2a7295635274489ad
cmtakahashi <ctakahashi08@gmail.com>
2021-11-28 21:24:05 -0800

    added class student

---
+++ Midterm/Midterm2/Students.hpp
@@ -0,0 +1,47 @@
+#ifndef STUDENTS_H
+#define STUDENTS_H
+
+#include <string>
+using namespace std;
+
+class Student
+{
+    private:
+        int sid;
+        string sname;
+        double score[3];
+
+    public:
+    int getSid() const
+    {
+        return sid; 
+    }
+    string getSname() const{
+        return sname;
+    }
+    double getScores(int i) const
+    {
+        return score[i];
+    }
+    double getSum() const{
+        return (score[0] + score[1] + score[2]);
+    }
+    double getAvg() const{
+        return (score[0] + score[1] + score[2]) / 3;
+    }
+    void setSid(int x) 
+    {
+        sid = x;
+    }
+    void setSname(string s)
+    {
+        sname = s;
+    }
+    void setScores(int idx,int value)
+    {
+        score[idx] = value; 
+    }
+
+};
+
+#endif
