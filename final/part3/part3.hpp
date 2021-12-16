6259228e7a4dfa8b48c485013ec5ee6c320c1494
cmtakahashi <ctakahashi08@gmail.com>
2021-12-15 22:14:24 -0800

    adding function definitions

--- final/part3/part3.hpp
+++ final/part3/part3.hpp
@@ -29,5 +29,42 @@ class Employee{
         string getHireDate(){
             return hireDate;
         }
+};
+
+class ProductionWorker: public Employee{
+    private:
+        int shift; // int value represents the shift the employee works (day = 1, night = 2)
+        double pay;
+
+    public:
+        ProductionWorker() : Employee(){
+            shift = 0;
+            pay = 0;
+        }
+
+        ProductionWorker(string name, int number, string hireDate, int shiftValue, double payRate) : Employee(name, number, hireDate){
+            shift = shiftValue;
+            pay = payRate;
+        }
+        
+        int getShift(){
+            return shift;
+        }
+
+        double getPay(){
+            return pay;
+        }
+
+        void printAllInfo(ProductionWorker w[], int N){
+            for(int i = 0; i < N; i++){
+                cout << w[i].getName();
+                cout << w[i].getNumber();
+                cout << w[i].getHireDate();
+                cout << w[i].getShift();
+                cout << w[i].getPay();
+            }
+
+        }
+
 
 };
