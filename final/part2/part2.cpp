587715d7b8fe753413a73db53f3aef4ee8ed299a
cmtakahashi <ctakahashi08@gmail.com>
2021-12-15 21:01:32 -0800

    copied code from part 1 to part 2

---
+++ final/part2/part2.cpp
@@ -0,0 +1,56 @@
+#include "part2.hpp"
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+
+int main(){
+     // test data from file 
+     // 10 courses
+     // sort courses based on id
+     // create binary seach function to search by ID
+
+    ifstream data_file("courses.txt");
+    if(data_file.fail()){
+        cout << "Error encountered when attempting to open the file!" << endl;
+        return 1;
+    }
+
+    const int N = 10;
+    int ID;
+    string name;
+    int credit;
+    Course courses[N];
+    int n = 0;
+    int target;
+
+    while (!data_file.eof() && n<N){
+        data_file >> ID;
+        data_file >> name;
+        data_file >> credit;
+
+        Course c;
+        c.setID(ID);
+        c.setName(name);
+        c.setCredit(credit);
+
+        courses[n] = c;
+        n++;
+    }
+
+    data_file.close();
+    
+    //print the initial course info to confirm correct processing
+    cout << endl << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
+    cout << endl << "List of Courses: " << endl;
+    printCourseInfo(courses, N);
+
+    bubbleSort(courses, N);
+    cout << endl;
+    cout << "Sorting courses by ID..." << endl;
+    cout << "List of sorted courses: " << endl;
+
+    printCourseInfo(courses, N);    
+    
+}
---
+++ final/part2/part2.hpp
@@ -0,0 +1,92 @@
+#ifndef PART2_H
+#define PART2_H
+#include <iostream>
+
+using namespace std;
+
+class Course{
+    private:
+        int ID;
+        string name;
+        int credit;
+
+    public:
+
+       void setID(int x){
+           ID = x;
+       };
+       void setName(string n){
+           name = n;
+       };
+       void setCredit(int c){
+           credit = c;
+       };
+
+       int getID(){
+           return ID;
+       };
+       string getName(){
+           return name;
+       };
+       int getCredit(){
+           return credit;
+       };
+
+};
+
+void bubbleSort (Course c[], int N){
+    for (int i=0; i < N; i++){
+        for (int j =0; j < N; j++){
+            if (c[i].getID() < c[j].getID()){
+                swap(c[j], c[i]);
+            }
+        }
+    }
+}
+
+void printCourseInfo(Course c[], int N){
+    cout << endl;
+    cout << "ID\t" << "Couse\t" << "Credits\t"  << endl;
+    cout << "====================================" << endl;
+
+    for (int i =0; i < N; i++){
+        cout << c[i].getID() << "\t" << c[i].getName() << "\t" << c[i].getCredit() << endl;
+        
+    }
+}
+
+
+int binarySearch(Course c[], int N, int target){
+    int first, last, mid;
+    bool found = false;
+    first = 0;
+    last = N-1;
+    mid = (first+last/2);
+
+    while (first <= last){
+        
+        mid = (first+last)/2;
+
+        if (c[mid].getID() == target){
+            found = true;
+            return mid;
+        }
+
+        else if(c[mid].getID() < target){
+            first = mid + 1;
+        }
+
+        else {
+            last = mid-1;
+        }
+    }
+    if (!found)
+        return -1;
+    else  
+        return mid;
+}
+
+
+
+
+#endif
