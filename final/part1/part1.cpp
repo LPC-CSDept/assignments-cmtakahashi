b532017307d2b1b3865aba667fcf2ff97af6b964
cmtakahashi <ctakahashi08@gmail.com>
2021-12-15 20:12:14 -0800

    added changes to read and print the file

--- final/part1/part1.cpp
+++ final/part1/part1.cpp
@@ -9,8 +9,36 @@ int main(){
      // sort courses based on id
      // create binary seach function to search by ID
 
-    
+    ifstream data_file("courses.txt");
+    if(data_file.fail()){
+        cout << "Error encountered when attempting to open the file!" << endl;
+        return 1;
+    }
 
+    const int N = 10;
+    int ID;
+    string name;
+    int credit;
+    Course courses[N];
+    int n = 0;
+
+    while (!data_file.eof() && n<N){
+        data_file >> ID;
+        data_file >> name;
+        data_file >> credit;
+
+        Course c;
+        c.setID(ID);
+        c.setName(name);
+        c.setCredit(credit);
+
+        courses[n] = c;
+        n++;
+    }
+
+    data_file.close();
+
+    printCourseInfo(courses, N);
 
 
 }
--- final/part1/part1.hpp
+++ final/part1/part1.hpp
@@ -31,3 +31,14 @@ class Course{
        };
 
 };
+
+void printCourseInfo(Course c[], int N){
+    cout << endl;
+    cout << "ID\t" << "Couse\t" << "Credits\t"  << endl;
+    cout << "====================================" << endl;
+
+    for (int i =0; i < N; i++){
+        cout << c[i].getID() << "\t" << c[i].getName() << "\t" << c[i].getCredit() << endl;
+        
+    }
+}
