3a63ecc5288d44ccc2479da247218d0c36ffeba9
cmtakahashi <ctakahashi08@gmail.com>
2021-12-15 20:01:41 -0800

    added directions and cpp file

---
+++ final/part1/part1.cpp
@@ -0,0 +1,16 @@
+#include "part1.hpp"
+#include <iostream>
+#include <fstream>
+
+
+int main(){
+     // test data from file 
+     // 10 courses
+     // sort courses based on id
+     // create binary seach function to search by ID
+
+    
+
+
+
+}
